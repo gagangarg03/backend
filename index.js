92db86e040f232f0c2250e6bdc4d719c391371f4
Gagan Garg <gagangarg489@gmail.com>
2025-06-12 14:04:21 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/insta',(req,res)=>{
+    res.send('enter your username and password')
+})
+
+app.get('/youtube',(req,res)=>{
+    res.send('<h1>What do you like to watch</h1>')
+})
+
+app.listen(process.env.port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
